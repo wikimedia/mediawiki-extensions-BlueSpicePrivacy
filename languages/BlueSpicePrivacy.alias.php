65b928e2032578b514396f0c93049943e87c5d02
rvogel <vogel@hallowelt.biz>
2019-01-21 16:30:02 +0100

    Add alias files for specialpages and extend \BlueSpice\SpecialPage
    => Needs cherry-pick to REL1_31
    
    Change-Id: Ib410e570406a1e39b4b5b0244efe9848ce5511cc

---
+++ languages/BlueSpicePrivacy.alias.php
@@ -0,0 +1,15 @@
+<?php
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'PrivacyCenter' => [ 'Privacy Center' ],
+	'PrivacyAdmin' => [ 'Privacy Admin' ]
+];
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = [
+	'PrivacyCenter' => [ 'Privatsphäre-Center' ],
+	'PrivacyAdmin' => [ 'Privatsphäreeinstellungen' ]
+];
