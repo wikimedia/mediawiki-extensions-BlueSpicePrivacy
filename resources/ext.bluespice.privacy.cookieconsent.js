1db18acf21da2f2b851c83695c004643840a7518
Dejan Savuljesku <d.savuljesku@gmail.com>
2019-02-04 08:12:57 +0100

    Add description message + fix loading order
    Make sure cookieConsent handler is loaded when trying to instantiate it.
    
    Needs cherry-picking to REL1_31
    
    Change-Id: I34cc39cdd6d4fc2637a30c54fe647e633f055652
    ERM: #13374
    (cherry picked from commit 821b2da016f4e080bc3302d4de266785df8ce333)

--- resources/ext.bluespice.privacy.cookieconsent.js
+++ resources/ext.bluespice.privacy.cookieconsent.js
@@ -1,17 +1,23 @@
-$( function() {
+( function( mw, $, bs ) {
 	var handlerConfig = mw.config.get( "bsPrivacyCookieConsentHandlerConfig" );
-	var callback = handlerConfig.class;
+	mw.loader.using( handlerConfig.RLModule ).done( function() {
+		var callback = handlerConfig.class;
 
-	var func = bs.privacy.util.funcFromCallback( callback );
+		var func = bs.privacy.util.funcFromCallback( callback );
 
-	var handler = new func( {
-		cookieName: handlerConfig.cookieName,
-		cookieMap: handlerConfig.map
+		try {
+			var handler = new func( {
+				cookieName: handlerConfig.cookieName,
+				cookieMap: handlerConfig.map
+			} );
+		} catch( err ) {
+			return;
+		}
+
+		// Bind to change cookie settings footer link
+		$( 'body' ).on( 'click', '#bs-privacy-footer-change-cookie-settings', function() {
+			var openSettings = handler.settingsOpen;
+			openSettings.apply( handler );
+		} );
 	} );
-
-	// Bind to change cookie settings footer link
-	$( 'body' ).on( 'click', '#bs-privacy-footer-change-cookie-settings', function() {
-		var openSettings = handler.settingsOpen;
-		openSettings.apply( handler );
-	} );
-} );
+} )( mediaWiki, jQuery, blueSpice );
