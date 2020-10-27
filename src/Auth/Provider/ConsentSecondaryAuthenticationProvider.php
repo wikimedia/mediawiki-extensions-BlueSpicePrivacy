67b207419d3ebfd24f74e4d85383049fb833a461
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-10-27 13:48:13 +0100

    Add proper acceptance of terms check
    On account create, or first login, if account was created by another person
    
    [3.3]
    
    ERM 21251
    
    Change-Id: Ib5f64c5f1d4422ad5999724f9b3d9b9690876b66

---
+++ src/Auth/Provider/ConsentSecondaryAuthenticationProvider.php
@@ -0,0 +1,144 @@
+<?php
+namespace BlueSpice\Privacy\Auth\Provider;
+
+use BlueSpice\Privacy\Auth\Request\ConsentAuthenticationRequest;
+use BlueSpice\Privacy\Module\Consent;
+use MediaWiki\Auth\AbstractSecondaryAuthenticationProvider;
+use MediaWiki\Auth\AuthenticationRequest;
+use MediaWiki\Auth\AuthenticationResponse;
+use MediaWiki\Auth\TemporaryPasswordAuthenticationRequest;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\User\UserOptionsManager;
+use Message;
+use RequestContext;
+use User;
+
+class ConsentSecondaryAuthenticationProvider extends AbstractSecondaryAuthenticationProvider {
+	/**
+	 *
+	 * @param string $action
+	 * @param array $options
+	 * @return array
+	 */
+	public function getAuthenticationRequests( $action, array $options ) {
+		return [];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function beginSecondaryAuthentication( $user, array $reqs ) {
+		if ( !$this->userAccepted( $user ) ) {
+			return $this->returnUI();
+		}
+
+		return AuthenticationResponse::newAbstain();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function continueSecondaryAuthentication( $user, array $reqs ) {
+		return $this->saveUserPrefs( $user, $reqs );
+	}
+
+	/**
+	 * @param User $user
+	 * @param User $creator
+	 * @param array $reqs
+	 * @return AuthenticationResponse
+	 */
+	public function beginSecondaryAccountCreation( $user, $creator, array $reqs ) {
+		$temporary = AuthenticationRequest::getRequestByClass(
+			$reqs, TemporaryPasswordAuthenticationRequest::class
+		);
+		if ( $temporary || !$creator->isAnon() ) {
+			// If someone else creates account, or used temporary password
+			// we don't want that user to be able to accept for the target user
+			return AuthenticationResponse::newAbstain();
+		}
+		if ( !$this->userAccepted( $user ) ) {
+			return $this->returnUI();
+		}
+
+		return AuthenticationResponse::newAbstain();
+	}
+
+	/**
+	 * @param User $user
+	 * @param User $creator
+	 * @param array $reqs
+	 * @return AuthenticationResponse|void
+	 */
+	public function continueSecondaryAccountCreation( $user, $creator, array $reqs ) {
+		return $this->saveUserPrefs( $user, $reqs );
+	}
+
+	/**
+	 * @param User $user
+	 * @param array $reqs
+	 * @return AuthenticationResponse
+	 */
+	protected function saveUserPrefs( User $user, array $reqs ) {
+		/** @var ConsentAuthenticationRequest $request */
+		$request = AuthenticationRequest::getRequestByClass(
+			$reqs,
+			ConsentAuthenticationRequest::class
+		);
+
+		if ( !$request ) {
+			return AuthenticationResponse::newAbstain();
+		}
+
+		$module = $this->getModule();
+		foreach ( $module->getOptions() as $name => $prefName ) {
+			if ( $request->$name === false ) {
+				return AuthenticationResponse::newFail(
+					Message::newFromKey( 'bs-privacy-consent-auth-fail' )
+				);
+			}
+			$this->getOptionsManager()->setOption( $user, $prefName, true );
+		}
+
+		$user->saveSettings();
+		return AuthenticationResponse::newPass( $user->getName() );
+	}
+
+	/**
+	 * @param User $user
+	 * @return bool
+	 */
+	protected function userAccepted( User $user ) {
+		$module = $this->getModule();
+		foreach ( $module->getOptions() as $name => $prefName ) {
+			if ( !$this->getOptionsManager()->getOption( $user, $prefName, false ) ) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @return UserOptionsManager
+	 */
+	protected function getOptionsManager() {
+		return MediaWikiServices::getInstance()->getUserOptionsManager();
+	}
+
+	/**
+	 * @return Consent
+	 */
+	protected function getModule() {
+		return new Consent( RequestContext::getMain() );
+	}
+
+	/**
+	 * @return AuthenticationResponse
+	 */
+	protected function returnUI() {
+		$request = new ConsentAuthenticationRequest();
+		return AuthenticationResponse::newUI( [ $request ],
+			Message::newFromKey( 'bs-privacy-consent-auth-step' ) );
+	}
+}
