6e962419c00941880aee8df160ce7b44bbbfef20
pwirth <wirth@hallowelt.biz>
2021-10-06 12:21:10 +0200

    Fix actor table not beeing skiped on deletion request
    ERM:25842
    [REL1_35-4.0.x]
    
    Change-Id: I5fc723c7a73f0dadaa31d8b0b6009a3c120d291f

--- src/Handler/Anonymize.php
+++ src/Handler/Anonymize.php
@@ -82,6 +82,9 @@ class Anonymize implements IPrivacyHandler {
 			if ( $table === 'user' && $this->skipUserTable ) {
 				continue;
 			}
+			if ( $table === 'actor' && $this->skipUserTable ) {
+				continue;
+			}
 			$this->db->update(
 				$table,
 				[ $field => $newUsername ],
