c1571d5046f54b1342a1edea7e2aec3ac4c534e2
miriam schlindwein <schlindwein@hallowelt.com>
2021-12-16 09:26:32 +0100

    Fix tab selection log in
    ERM27004
    
    [4.x]
    
    Change-Id: I4fc2732b8221bf212956e7938d52bd2fe5b3dac4

---
+++ resources/ext.bluespice.privacy.focusTrap.cookieProvider.js
@@ -0,0 +1,23 @@
+( function( mw, $, d ){
+
+	$( d ).keydown( function(e) {
+		if( e.key.toLowerCase() === 'tab' &&
+			$( '.bs-privacy-cookie-consent-mw-provider-bar' ).length ) {
+			var $targetEl = $( e.target );
+			var $tabChildren = $( ".bs-privacy-cookie-consent-mw-provider-bar ").children();
+
+			if( e.shiftKey ) {
+				if ( $targetEl.is( $tabChildren.first() ) ) {
+					e.preventDefault();
+					$tabChildren.last().children().focus();
+				}
+			} else {
+				if ( $targetEl.is( $tabChildren.last().children() ) ) {
+					e.preventDefault();
+					$tabChildren.first().focus();
+				}
+			}
+		}
+	})
+
+} )( mediaWiki, jQuery, document );
