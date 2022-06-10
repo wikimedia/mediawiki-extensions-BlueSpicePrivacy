7f540dd3dd52255a9acf920b2b2b12386f97ac78
d.savuljesku <d.savuljesku@gmail.com>
2022-06-10 10:23:17 +0200

    Allow disabling of acepting PP during login process
    [4.1.x]
    
    ERM 28808
    
    Change-Id: Ifa1f178511a4c442f894bab30aac23a77c81fed2

--- src/Extension.php
+++ src/Extension.php
@@ -4,10 +4,12 @@ namespace BlueSpice\Privacy;
 
 class Extension extends \BlueSpice\Extension {
 	public static function onCallback() {
-		$GLOBALS[ 'wgAuthManagerAutoConfig' ][ 'secondaryauth' ]
-		[ Auth\Provider\ConsentSecondaryAuthenticationProvider::class ] = [
-			'class' => Auth\Provider\ConsentSecondaryAuthenticationProvider::class
-		];
+		if ( $GLOBALS['bsgPrivacyPrivacyPolicyOnLogin'] ) {
+			$GLOBALS[ 'wgAuthManagerAutoConfig' ][ 'secondaryauth' ]
+			[ Auth\Provider\ConsentSecondaryAuthenticationProvider::class ] = [
+				'class' => Auth\Provider\ConsentSecondaryAuthenticationProvider::class
+			];
+		}
 
 		$GLOBALS['wgLogRestrictions']['bs-privacy'] = 'bs-privacy-admin';
 
