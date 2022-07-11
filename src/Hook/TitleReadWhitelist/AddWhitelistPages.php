b1094eb873890b7bc9a8cbd57183f233dc079e87
rvogel <vogel@hallowelt.biz>
2022-07-11 11:39:30 +0200

    Fix terms of service and privacy policy issues
    ERM23732
    
    [4.2.x]
    
    Change-Id: I9ddac61f13877a8b3c2b1285032e6a19e9795e15

---
+++ src/Hook/TitleReadWhitelist/AddWhitelistPages.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BlueSpice\Privacy\Hook\TitleReadWhitelist;
+
+use BlueSpice\Hook\TitleReadWhitelist;
+use Title;
+
+class AddWhitelistPages extends TitleReadWhitelist {
+
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function skipProcessing() {
+		$pages = [
+			'bs-privacy-privacypage',
+			'bs-privacy-termsofservicepage',
+			'privacypages'
+		];
+
+		foreach ( $pages as $value ) {
+			$page = $this->msg( $value );
+			$title = Title::newFromText( $page->plain() );
+			if ( $title && $this->title->equals( $title ) ) {
+				return false;
+			}
+			$title = Title::newFromText( $page->inContentLanguage()->plain() );
+			if ( $title && $this->title->equals( $title ) ) {
+				return false;
+			}
+
+		}
+	}
+
+	protected function doProcess() {
+		$this->whitelisted = true;
+	}
+}
