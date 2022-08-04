174d1d3c736b64b2f4556232127d63f25c3ce753
pavloyashchenko <pyashchenko@hallowelt.com>
2022-08-04 15:45:11 +0200

    Create AbstractSchema for sql table
    Create Json file; Generate 3 files(sqlite, postgres, mysql); Change path in addExtensionTable() to generated file.
    
    ERM29227
    
    [master only]
    
    Change-Id: I9310b03b0a088f11384a9aab0d41b32ba2ade6e1

--- src/Hook/LoadExtensionSchemaUpdates/AddPrivacyRequestTable.php
+++ src/Hook/LoadExtensionSchemaUpdates/AddPrivacyRequestTable.php
@@ -6,11 +6,12 @@ use BlueSpice\Hook\LoadExtensionSchemaUpdates;
 
 class AddPrivacyRequestTable extends LoadExtensionSchemaUpdates {
 	protected function doProcess() {
+		$dbType = $this->updater->getDB()->getType();
 		$dir = $this->getExtensionPath() . '/maintenance/db';
 
 		$this->updater->addExtensionTable(
 			'bs_privacy_request',
-			"$dir/bs_privacy_request.sql"
+			"$dir/sql/$dbType/bs_privacy_request-generated.sql"
 		);
 	}
 
