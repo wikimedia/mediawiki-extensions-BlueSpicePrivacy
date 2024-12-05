5f3b371c8aaa543762eeef932b8d18c0a317cd85
d.savuljesku <d.savuljesku@gmail.com>
2024-12-05 11:13:09 +0100

    Feedback on new UI
    [5.0]
    
    ERM39446
    
    Change-Id: I168b9075f4955c9636d26d6cb7e82741f22a0a5b
    (cherry picked from commit d6162d876e48896b89ec5ba6fb95a7d90988a1c6)

--- resources/ext.bluespice.privacy.admin.js
+++ resources/ext.bluespice.privacy.admin.js
@@ -2,28 +2,33 @@
 	var requestManager = new bs.privacy.widget.RequestManager( {
 		$element: $( '#bs-privacy-admin-requests' )
 	} );
+	requestManager.init();
+
+	function initPanel( $section ) {
+		var sectionCallback = $section.data( 'callback' );
+		var func = bs.privacy.util.funcFromCallback( sectionCallback );
+
+		var config = {};
+		if( $section.data( 'config' ) ) {
+			config = $section.data( 'config' );
+		}
+
+		var widget = new func( $.extend( {
+			$element: $section
+		}, config ) );
+		widget.init();
+	}
 
 	$( '.bs-privacy-admin-section' ).each( function( k, section ) {
 		var $section = $( section );
 
 		var rlModule = $section.data( 'rl-module' );
 		if ( !rlModule ) {
+			initPanel( $section );
 			return;
 		}
-
 		mw.loader.using( rlModule ).then( function() {
-			var sectionCallback = $section.data( 'callback' );
-			var func = bs.privacy.util.funcFromCallback( sectionCallback );
-
-			var config = {};
-			if( $section.data( 'config' ) ) {
-				config = $section.data( 'config' );
-			}
-
-			var widget = new func( $.extend( {
-				$element: $section
-			}, config ) );
-			widget.init();
+			initPanel( $section );
 		} );
 	} );
 } )( mediaWiki, jQuery );
